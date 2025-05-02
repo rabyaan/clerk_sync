8625a7df403f29c0d51ff972afb05b200dc68951
unknown <rabyaan.m@gmail.com>
2025-05-03 01:07:42 +0500

    fix webhooks

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -3,7 +3,7 @@ import { SignIn } from "@clerk/nextjs";
 export default function Page() {
   return (
   <div className="flex flex-col items-center justify-center h-screen ">
-    <h1 className="text-3xl font-bold mb-4">Sign In</h1>
+    <h1 className="text-3xl font-bold mb-4">Sign In </h1>
     <SignIn />
   </div>)
 
