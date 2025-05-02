feebcce2a5859a3450f206a72d1a499783446eab
unknown <rabyaan.m@gmail.com>
2025-05-03 00:27:39 +0500

    fix webhooks

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -1,20 +1,25 @@
-import { verifyWebhook } from '@clerk/nextjs/webhooks'
+import { verifyWebhook } from "@clerk/nextjs/webhooks";
 import { NextRequest } from "next/server";
 
 export async function POST(req: NextRequest) {
   try {
-    const evt = await verifyWebhook(req)
+    const evt = await verifyWebhook(req);
 
     // Do something with payload
     // For this guide, log payload to console
-    const { id } = evt.data
-    const eventType = evt.type
-    console.log(`Received webhook with ID ${id} and event type of ${eventType}`)
-    console.log('Webhook payload:', evt.data)
+    const { id } = evt.data;
+    const eventType = evt.type;
 
-    return new Response('Webhook received', { status: 200 })
+    console.log(`Received webhook with ID ${id} and event type of ${eventType}`);
+    console.log("Webhook payload:", evt.data);
+
+    if (evt.type === "user.created") {
+      console.log("userId:", evt.data.id);
+    }
+
+    return new Response("Webhook received", { status: 200 });
   } catch (err) {
-    console.error('Error verifying webhook:', err)
-    return new Response('Error verifying webhook', { status: 400 })
+    console.error("Error verifying webhook:", err);
+    return new Response("Error verifying webhook", { status: 400 });
   }
 }
